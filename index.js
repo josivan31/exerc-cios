1baeb4d69c31ad72ff82f50c771c0cbecf2e27b2
JOSIVANSANTOS2 <89109787+josivan31@users.noreply.github.com>
2021-08-19 02:01:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let titulo = "programação";
+let linguagem = " em Javascript";
+//console.log(titulo+linguagem); //concatenação no console log 
+let fusao = titulo + linguagem;
+console.log(fusao);
+
+// length ->retorna o tamanho da mensagem
+let mensagem = "olá, Bluemers";
+console.log(`O tamanho da String mensagem é ${mensagem.length}`)
+
+console.log(mensagem.length);
